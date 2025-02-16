ebef6ba303a17d0f404f57bad9d35305a564c363
swapnil20711 <swapnil.bhojwani@gmail.com>
2025-02-16 16:02:39 +0530

    custom map function added

--- hof/index.js
+++ hof/index.js
@@ -32,3 +32,19 @@ const diameter = calculate(radius, (radii) => {
 console.log("Area of circles is : ", area);
 console.log("Circumference of circles is : ", circumference);
 console.log("Diameter of circles is : ", diameter);
+
+//Our custom map implementations
+const array = [3, 2, 1, 4];
+Array.prototype.customMap = function (transform) {
+  let output = [];
+  for (let i = 0; i < this.length; i++) {
+    output.push(transform(this[i]));
+  }
+  return output;
+};
+
+const squares = array.customMap((element) => {
+  return element * element;
+});
+
+console.log("Squares of array is : ", squares);
